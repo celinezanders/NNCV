f5e339ac5e8561d0cd825d524a8152f8fff9b25a
celinezanders <c.i.zanders@student.tue.nl>
2025-03-18 16:33:19 +0100

    docker
    z

--- Final assignment/download_docker_and_data.sh
+++ Final assignment/download_docker_and_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=9
+#SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
-#SBATCH --partition=gpu_mig
+#SBATCH --partition=gpu_a100
 #SBATCH --time=3:00:00
 
 # Pull container from dockerhub
