d243b88216477f13c792946c4fc858da922a3527
celinezanders <c.i.zanders@student.tue.nl>
2025-04-02 21:24:03 +0200

    Update train.py

--- Final assignment/train.py
+++ Final assignment/train.py
@@ -26,6 +26,21 @@ id_to_trainid = {cls.id: cls.train_id for cls in Cityscapes.classes}
 def convert_to_train_id(label_img: torch.Tensor) -> torch.Tensor:
     return label_img.apply_(lambda x: id_to_trainid[x])
 
+# Mapping train IDs to color
+train_id_to_color = {cls.train_id: cls.color for cls in Cityscapes.classes if cls.train_id != 255}
+train_id_to_color[255] = (0, 0, 0)  # Assign black to ignored labels
+
+def convert_train_id_to_color(prediction: torch.Tensor) -> torch.Tensor:
+    batch, _, height, width = prediction.shape
+    color_image = torch.zeros((batch, 3, height, width), dtype=torch.uint8)
+
+    for train_id, color in train_id_to_color.items():
+        mask = prediction[:, 0] == train_id
+        for i in range(3):
+            color_image[:, i][mask] = color[i]
+
+    return color_image
+
 def get_args_parser():
     parser = ArgumentParser("Training script for a PyTorch U-Net model")
     parser.add_argument("--data-dir", type=str, default="./data/cityscapes", help="Path to the training data")
