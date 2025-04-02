f1e509cdea14755126a563c078676029ce00723f
celinezanders <c.i.zanders@student.tue.nl>
2025-04-02 14:27:09 +0200

    Update unet.py

--- Final assignment/unet.py
+++ Final assignment/unet.py
@@ -3,8 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-#deze nog naar kijken dit word ingeleverd
-
 class Model(nn.Module):
     """ 
     A simple U-Net architecture for image segmentation.
@@ -13,19 +11,18 @@ class Model(nn.Module):
     https://arxiv.org/pdf/1505.04597.pdf
     """
     def __init__(self, in_channels=3, n_classes=1):
-        
         super(Model, self).__init__()
 
-        self.inc = (DoubleConv(in_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
-        self.down4 = (Down(512, 1024)) # veranderd naar 1024 -> meer filters dus krachtiger
-        self.up1 = (Up(1024 + 512, 512)) # up krijgt nu 1024 van encoder en 512 van skip connection
-        self.up2 = (Up(512 + 256, 256))
-        self.up3 = (Up(256 + 128, 128))
-        self.up4 = (Up(128 + 64, 64))
-        self.outc = (OutConv(64, n_classes))
+        self.inc = DoubleConv(in_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)  # Changed to 1024 filters for more power
+        self.up1 = Up(1024 + 512, 512)  # Up gets 1024 from encoder and 512 from skip connection
+        self.up2 = Up(512 + 256, 256)
+        self.up3 = Up(256 + 128, 128)
+        self.up4 = Up(128 + 64, 64)
+        self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -40,22 +37,20 @@ class Model(nn.Module):
         logits = self.outc(x)
 
         return logits
-        
+
 
 class DoubleConv(nn.Module):
-    #"""(convolution => [BN] => ReLU) * 2"""
-    """(convolution => [BN] => ReLU) * 2 with dropout"""
+    """(convolution => [GroupNorm] => ReLU) * 2 with dropout"""
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
+            nn.GroupNorm(32, mid_channels),  # Replaced BatchNorm with GroupNorm
             nn.ReLU(inplace=True),
-            nn.Dropout(0.1), #dit toegevoegd om het per layer te contorleren
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
+            nn.GroupNorm(32, out_channels),  # Replaced BatchNorm with GroupNorm
             nn.ReLU(inplace=True)
         )
 
@@ -64,45 +59,37 @@ class DoubleConv(nn.Module):
 
 
 class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
+    """Downscaling with strided convolution"""
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.GroupNorm(32, out_channels),
+            nn.ReLU(inplace=True),
         )
 
     def forward(self, x):
-        return self.maxpool_conv(x)
+        return self.conv(x)
 
 
 class Up(nn.Module):
-    """Upscaling then double conv"""
-
+    """Upscaling using ConvTranspose2d (deconvolution)"""
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
-       # self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-       # self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-      
-#dit aangepast hier kist het tussen bilinear upsampling (geen parameters) of een transposed convulution (wel leerbare parameters)
         if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
         else:
-            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
+            self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
 
         self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-
-#dit toegevoegd voor size mathcing met afrondingen
+        # Adjust the size of x1 to match x2
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        
-        x = torch.cat([x2, x1], dim=1)
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        x = x2 + x1  # Element-wise addition for skip connections
         return self.conv(x)
 
 
